d12ad7b592bf6496cc611b7bd14963beae45da8e
KeyzarRasya <keyzar.r.athallah@gmail.com>
2023-10-30 10:39:02 +0700

    SecurityConfig commit

--- src/main/java/enviro/fund/production/config/SecurityConfig.java
+++ src/main/java/enviro/fund/production/config/SecurityConfig.java
@@ -1,61 +1,61 @@
-//package enviro.fund.production.config;
-//
-//import enviro.fund.production.model.User;
-//import enviro.fund.production.service.UserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.AuthenticationProvider;
-//import org.springframework.security.authentication.ProviderManager;
-//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.core.context.SecurityContextHolderStrategy;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//
-//import java.security.Provider;
-//
-//@EnableWebSecurity
-//@EnableWebMvc
-//@Configuration
-//public class SecurityConfig {
-//
-//    @Bean
-//    UserDetailsService userDetailsService(){
-//        return new UserService();
-//    }
-//    @Bean
-//    DaoAuthenticationProvider daoAuthenticationProvider(){
-//        DaoAuthenticationProvider dao = new DaoAuthenticationProvider();
-//        dao.setPasswordEncoder(passwordEncoder());
-//        dao.setUserDetailsService(userDetailsService());
-//        return dao;
-//    }
-//
-//    @Bean
-//    PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
-//    @Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-//        return http
-//                .csrf(csrf -> csrf.disable())
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/charity/create").authenticated()
-//                        .requestMatchers("/user/donate").authenticated()
-//                        .requestMatchers("/user/all").authenticated()
-//                        .requestMatchers("/charity/verify/**").hasRole("ADMIN")
-//                        .anyRequest().permitAll())
-//                .sessionManagement(session -> session.maximumSessions(1))
-//                .formLogin(Customizer.withDefaults())
-//                .httpBasic(Customizer.withDefaults())
-//                .build();
-//    }
-//}
+package enviro.fund.production.config;
+
+import enviro.fund.production.model.User;
+import enviro.fund.production.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.security.Provider;
+
+@EnableWebSecurity
+@EnableWebMvc
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    UserDetailsService userDetailsService(){
+        return new UserService();
+    }
+    @Bean
+    DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider dao = new DaoAuthenticationProvider();
+        dao.setPasswordEncoder(passwordEncoder());
+        dao.setUserDetailsService(userDetailsService());
+        return dao;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/charity/create").authenticated()
+                        .requestMatchers("/user/donate").authenticated()
+                        .requestMatchers("/user/all").authenticated()
+                        .requestMatchers("/charity/verify/**").hasRole("ADMIN")
+                        .anyRequest().permitAll())
+                .sessionManagement(session -> session.maximumSessions(1))
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
